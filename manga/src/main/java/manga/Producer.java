592d94e7095eb641774619ca2b4b83c11ffa45b0
megaNour <alkotobnour@gmail.com>
2017-12-01 01:57:40 +0100

    in...

--- manga/src/main/java/manga/Producer.java
+++ manga/src/main/java/manga/Producer.java
@@ -4,6 +4,9 @@ public class Producer {
 
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
+		if(args.length == 0) {
+			System.out.println("batch all files");
+		}
 	}
 
 }
